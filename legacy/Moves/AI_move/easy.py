0c7c39f9da37c88f9b70ebebd885a9a0f83aec14
Ahmed ali <ahmedmihamed827@gmail.com>
2026-01-13 00:36:01 +0200

    Cleanup: Remove legacy code

--- legacy/Moves/AI_move/easy.py
+++
@@ -1,6 +0,0 @@
-import random
-def make_ai_move_easy(board):
-    empty_cells = [(row, col) for row in range(3) for col in range(3) if board[row][col] == " "]
-    if empty_cells:
-        return random.choice(empty_cells)
-    return None
